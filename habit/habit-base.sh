179a1b5c27200a2571e4b1d8640a57d69d98210b
Harald Barth <haba@kth.se>
2026-06-12 15:03:42 +0200

    base pkg

--- habit/habit-base.sh
+++ habit/habit-base.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+if test $BASEINSTALL ; then
+    apt install xev
+    apt install mint-meta-xfce
+    apt install emacs
+    apt install emacs
+    apt install sshd
+    apt install openssh-server
+    apt install heimdal-clients
+    apt install xfce4-terminal
+    apt install xfce4-power-manager xfce4-power-manager-plugins
+    apt install xfce4-volumed-pulse
+    apt install xfce4-pulseaudio-plugin
+fi
+
 if test $OPENAFS ; then
+
+    apt install linux-headers-7.0.0-14-generic
+    apt install linux-image-7.0.0-14-generic
+    
     PACKETLIST = "openafs-client_1.8.16~pre1-1_amd64.deb openafs-krb5_1.8.16~pre1-1_amd64.deb"
     URL=http://ftp.se.debian.org/debian/pool/main/o/openafs/
     for PACKET in $PACKETLIST  ; do
@@ -13,6 +31,8 @@ AFS_SETCRYPT=on
 AFS_SYSNAME=
 KMOD=openafs
 EOF
+    #dkms status should give
+    # openafs/1.8.16pre1, 7.0.0-14-generic .... installed
 fi # openafs
 
 if test $KEYD ; then
